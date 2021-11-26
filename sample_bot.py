bd52d54ac85702789d4284a5245603ae9fcd3b90
a.myronyuk <a.myronyuk@smartweb.com.ua>
2021-11-26 21:50:55 +0200

    Upd readme

--- sample_bot.py
+++ sample_bot.py
@@ -1,62 +1,52 @@
-from flask import Flask, request, Response
+from aiohttp import web
+
 from aioviberbot import Api
 from aioviberbot.api.bot_configuration import BotConfiguration
 from aioviberbot.api.messages.text_message import TextMessage
 from aioviberbot.api.viber_requests import ViberConversationStartedRequest
-from aioviberbot.api.viber_requests import ViberFailedRequest
 from aioviberbot.api.viber_requests import ViberMessageRequest
 from aioviberbot.api.viber_requests import ViberSubscribedRequest
-from aioviberbot.api.viber_requests import ViberUnsubscribedRequest
 
-import time
-import logging
-import sched
-import threading
-
-logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-handler = logging.StreamHandler()
-formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-handler.setFormatter(formatter)
-logger.addHandler(handler)
-
-app = Flask(__name__)
 viber = Api(BotConfiguration(
-  name='PythonSampleBot',
-  avatar='http://viber.com/avatar.jpg',
-  auth_token='YOUR_AUTH_TOKEN_HERE'
+    name='PythonSampleBot',
+    avatar='http://viber.com/avatar.jpg',
+    auth_token='YOUR_AUTH_TOKEN_HERE'
 ))
 
-@app.route('/', methods=['POST'])
-def incoming():
-    logger.debug("received request. post data: {0}".format(request.get_data()))
 
-    viber_request = viber.parse_request(request.get_data().decode('utf8'))
+async def webhook(request: web.Request) -> web.Response:
+    request_data = await request.read()
+    signature = request.headers.get('X-Viber-Content-Signature')
+    if not viber.verify_signature(request_data, signature):
+        raise web.HTTPForbidden
 
+    viber_request = viber.parse_request(request_data)
     if isinstance(viber_request, ViberMessageRequest):
-        message = viber_request.message
-        viber.send_messages(viber_request.sender.id, [
-            message
-        ])
-    elif isinstance(viber_request, ViberConversationStartedRequest) \
-            or isinstance(viber_request, ViberSubscribedRequest) \
-            or isinstance(viber_request, ViberUnsubscribedRequest):
-        viber.send_messages(viber_request.sender.id, [
-            TextMessage(None, None, viber_request.get_event_type())
-        ])
-    elif isinstance(viber_request, ViberFailedRequest):
-        logger.warn("client failed receiving message. failure: {0}".format(viber_request))
+        # echo message
+        await viber.send_messages(
+            viber_request.sender.id,
+            [viber_request.message],
+        )
+    elif isinstance(viber_request, ViberSubscribedRequest):
+        await viber.send_messages(
+            viber_request.user.id,
+            [TextMessage(text='Thanks for subscribing!')],
+        )
+    elif isinstance(viber_request, ViberConversationStartedRequest):
+        await viber.send_messages(
+            viber_request.user.id,
+            [TextMessage(text='Thanks for starting conversation!')],
+        )
 
-    return Response(status=200)
+    return web.json_response({'ok': True})
 
-def set_webhook(viber):
-    viber.set_webhook('https://mybotwebserver.com:8443/')
 
-if __name__ == "__main__":
-    scheduler = sched.scheduler(time.time, time.sleep)
-    scheduler.enter(5, 1, set_webhook, (viber,))
-    t = threading.Thread(target=scheduler.run)
-    t.start()
+async def set_webhook_signal(app: web.Application):
+    await viber.set_webhook('https://mybotwebserver.com/webhhok')
 
-    context = ('server.crt', 'server.key')
-    app.run(host='0.0.0.0', port=8443, debug=True, ssl_context=context)
+
+if __name__ == '__main__':
+    app = web.Application()
+    app.on_startup.append(set_webhook_signal)
+    app.router.add_route('POST', '/webhook', webhook),
+    web.run_app(app)
